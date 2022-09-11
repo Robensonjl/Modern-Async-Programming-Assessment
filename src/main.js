c335f3e1849a216eb864a4b34246037e41173b91
Robenson <robenson76@gmail.com>
2022-09-10 19:46:10 -0500

    upload of completed assignment

--- src/main.js
+++ src/main.js
@@ -5,8 +5,31 @@ function isValid({ id, name, meaning, quadrant, starsWithPlanets }) {
   return id && name && meaning && quadrant && starsWithPlanets;
 }
 
-function update(constellation) {}
+async function update(constellation) {
+  try {
+    const url = `${BASE_URL}/constellations/${constellation.id}`;
+    const putUpdate = await axios.put(url, constellation);
 
-function bulkImport(constellations) {}
+    return putUpdate;
+  } catch (error) {
+    console.log(`Updating constellation (id: ${constellation.id}) failed.`);
+  }
+}
+
+async function bulkImport(constellations) {
+  try {
+    if (!Array.isArray(constellations))
+      throw newError("Inputted argument must be an array.");
+    if (!constellations.every((constellation) => isValid(constellation)))
+      throw newError("All constellations must include relevant fields.");
+
+    const imported = constellations.map((constellation) => {
+      return update(constellation);
+    });
+    return Promise.allSettled(imported);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = { bulkImport, update };
